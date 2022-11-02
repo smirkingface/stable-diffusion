08938ab8edf90fcc3537cac392596797fbd6f58d
SmirkingFace <116507648+smirkingface@users.noreply.github.com>
2022-11-02 20:37:36 +0100

    Fixed merge-ckpt to never merge scheduler keys. Added options to merge VAE and/or text model (default = not merged)

--- scripts/merge-ckpt.py
+++ scripts/merge-ckpt.py
@@ -3,8 +3,19 @@ import torch
 
 from sd.util import traverse_state_dict
 
-def merge_not_in_source(sd_src, sd_dst, k, state, verbose=True, alpha_new=0.5, **kwargs):
+scheduler_keys = ['betas', 'alphas_cumprod', 'alphas_cumprod_prev', 'sqrt_alphas_cumprod', 'sqrt_one_minus_alphas_cumprod', 'log_one_minus_alphas_cumprod', 'sqrt_recip_alphas_cumprod', 'sqrt_recipm1_alphas_cumprod', 'posterior_variance', 'posterior_log_variance_clipped', 'posterior_mean_coef1', 'posterior_mean_coef2']
+
+def merge_not_in_source(sd_src, sd_dst, k, state, verbose=True, alpha_new=0.5, merge_vae=False, merge_text_model=False, **kwargs):
     dst_item = sd_dst[k]
+    
+    if not merge_vae and k.startswith('first_stage_model.'):
+        return dst_item
+    if not merge_text_model and k.startswith('cond_stage_model.'):
+        return dst_item
+
+    # Ignore missing scheduler parameters in merging
+    if k in scheduler_keys:
+        return dst_item
 
     if len(dst_item.shape) == 1:
         if verbose:
@@ -17,7 +28,19 @@ def merge_not_in_source(sd_src, sd_dst, k, state, verbose=True, alpha_new=0.5, *
         torch.nn.init.dirac_(dst_item)
         return (1-alpha_new)*dst_item + tmp
 
-def merge_size_different(sd_src, sd_dst, k, state, verbose=True, alpha=0.5, alpha_new=0.5, **kwargs):
+def merge_not_in_dest(sd_src, sd_dst, k, state, verbose=True, **kwargs):
+    if k in scheduler_keys:
+        if verbose:
+            print(f'{k}: Scheduler parameter not in destination, adding')
+        return sd_src[k]
+    return None
+
+def merge_size_different(sd_src, sd_dst, k, state, verbose=True, alpha=0.5, alpha_new=0.5, merge_vae=False, merge_text_model=False, **kwargs):
+    if not merge_vae and k.startswith('first_stage_model.'):
+        return sd_dst[k]
+    if not merge_text_model and k.startswith('cond_stage_model.'):
+        return sd_dst[k]
+
     src_size = sd_src[k].size()
     dst_size = sd_dst[k].size()
     
@@ -32,18 +55,24 @@ def merge_size_different(sd_src, sd_dst, k, state, verbose=True, alpha=0.5, alph
     return (1-alpha) * pd(sd_src[k]) + mask*alpha*sd_dst[k] + alpha_new*(1-mask)*sd_dst[k]
 
 
-def merge_matching(sd_src, sd_dst, k, state, verbose=True, alpha=0.5, **kwargs):
+def merge_matching(sd_src, sd_dst, k, state, verbose=True, alpha=0.5, merge_vae=False, merge_text_model=False, **kwargs):
+    if not merge_vae and k.startswith('first_stage_model.'):
+        return sd_dst[k]
+    if not merge_text_model and k.startswith('cond_stage_model.'):
+        return sd_dst[k]
+
     if verbose:
         print(f'{k}: Merging')
     return (1-alpha) * sd_src[k] + alpha*sd_dst[k]
     
-def merge_checkpoint(src_filename, dst_filename, output_filename, alpha=0.5, alpha_new=0.5, verbose=True):
+def merge_checkpoint(src_filename, dst_filename, output_filename, alpha=0.5, alpha_new=0.5, merge_vae=False, merge_text_model=False, verbose=True):
     checkpoint_src = torch.load(src_filename, map_location='cpu')
     checkpoint_dst = torch.load(dst_filename, map_location='cpu')
     
     checkpoint_dst['state_dict'],_ = traverse_state_dict(checkpoint_src['state_dict'], checkpoint_dst['state_dict'], func_not_in_source=merge_not_in_source,
                                                          func_matching=merge_matching, func_size_different=merge_size_different,
-                                                         alpha=alpha, alpha_new=alpha_new, verbose=verbose)
+                                                         alpha=alpha, alpha_new=alpha_new, verbose=verbose,
+                                                         merge_vae=merge_vae, merge_text_model=merge_text_model)
     
     remove_keys = ['loops', 'optimizer_states', 'lr_schedulers', 'callbacks']
     for x in remove_keys:
@@ -89,11 +118,23 @@ if __name__ == '__main__':
         type=float,
         help='merge alpha (new weights)',
     )
-    
+    parser.add_argument(
+        '-mv',
+        '--merge_vae',
+        action='store_true',
+        help='merge VAE model (if false, uses the VAE in the dst checkpoint)'
+    )
+    parser.add_argument(
+        '-mt',
+        '--merge_text_model',
+        action='store_true',
+        help='merge text model (if false, uses the text model in the dst checkpoint)'
+    )
+        
     opt = parser.parse_args()
     
     alpha_new = opt.alpha_new
     if alpha_new == None:
         alpha_new = opt.alpha
 
-    merge_checkpoint(opt.src, opt.dst, opt.output_filename, alpha=opt.alpha, alpha_new=alpha_new)
+    merge_checkpoint(opt.src, opt.dst, opt.output_filename, alpha=opt.alpha, alpha_new=alpha_new, merge_vae=opt.merge_vae, merge_text_model=opt.merge_text_model)
